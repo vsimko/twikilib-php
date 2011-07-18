1671c327b6f857f4309d01d9cd90bb308f5bfa25
viliam.simko@gmail.com <viliam.simko@gmail.com@e8d5bb4a-7aaa-36a1-c079-f33c94fe2e21>
2011-07-18 15:59:54 +0000

    Added DefaultWrapFactory as an example of how topic wrappers can be used.

---
+++ trunk/twikilib/wrap/Group.php
@@ -0,0 +1,72 @@
+<?php
+namespace ciant\wrap;
+
+use twikilib\utils\System;
+use twikilib\core\ITopic;
+use twikilib\core\ITopicFactory;
+use ciant\wrap\ITopicWrapper;
+
+/**
+ * @author Viliam Simko
+ */
+class Group implements ITopicWrapper {
+	
+	private $users;
+	private $subgroups;
+	
+	/**
+	 * @var twikilib\core\ITopic
+	 */
+	private $wrappedTopic;
+		
+	/**
+	 * (non-PHPdoc)
+	 * @see ciant\wrap.ITopicWrapper::getWrappedTopic()
+	 */
+	public function getWrappedTopic() {
+		return $this->wrappedTopic;
+	}
+	
+	final public function __construct(ITopic $wrappedTopic) {
+		
+		$this->wrappedTopic = $wrappedTopic;
+		
+		$topicFactory = $wrappedTopic->getTopicFactory();
+		assert($topicFactory instanceof ITopicFactory);
+		
+		$this->users = array();
+		$this->subgroups = array();
+		
+		$topicVars = $wrappedTopic->getTopicTextNode()->getLocalVariablesFromText();
+		foreach($topicVars as $var) {
+			if($var->name == 'GROUP') {
+				$topicNames = explode(',', $var->value);
+				foreach($topicNames as $topicName) {
+					$topicName = trim($topicName);
+					//System::log($topicName);
+					$topic = $topicFactory->loadTopicByName($topicName);
+					$formName = $topic->getTopicFormNode()->getFormName();
+					
+					$normalizedTopicName = $topic->getTopicName();
+					
+					if($formName == 'UserForm') {
+						$this->users[ $normalizedTopicName ] = $topic;
+					} else {
+						$this->subgroups[ $normalizedTopicName ] = new Group($topic);
+						$this->users = array_merge($this->users, $this->getGroupUsers());
+					}
+					
+				}
+			}
+		}
+	}
+	
+	/**
+	 * List of all users extracted using a transitive closure.
+	 * @return array
+	 */
+	final public function getGroupUsers() {
+		return $this->users;
+	}
+}
+?>

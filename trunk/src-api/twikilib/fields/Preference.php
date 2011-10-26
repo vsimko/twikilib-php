5cbb4e88df28bce8212eb124a6d9ecf0cfb90e3e
viliam.simko@gmail.com <viliam.simko@gmail.com@e8d5bb4a-7aaa-36a1-c079-f33c94fe2e21>
2011-10-26 17:45:51 +0000

    minor updates

--- trunk/src-api/twikilib/fields/Preference.php
+++ trunk/src-api/twikilib/fields/Preference.php
@@ -18,21 +18,39 @@ class Preference implements IRenderable {
 		$this->args = (object) $args;
 	}
 	
+	/**
+	 * @return string
+	 */
 	final public function toWikiString() {
 		return Encoder::createWikiTag('META:PREFERENCE', $this->args)."\n";
 	}
 	
+	/**
+	 * @return string
+	 */
 	final public function __toString() {
 		assert(/* conversion to string not supported */);
 	}
 	
+	/**
+	 * @return string
+	 */
 	final public function getName() {
 		return $this->args->name;
 	}
 	
+	/**
+	 * @return string
+	 */
 	final public function getValue() {
 		return $this->args->value;
 	}
 	
+//	/**
+//	 * @param array $regexDelimiter
+//	 */
+//	final public function getValueAsList( $regexDelimiter = '/[,;] */') {
+//		return preg_split($regexDelimiter, $this->getValue());
+//	}
 }
 ?>

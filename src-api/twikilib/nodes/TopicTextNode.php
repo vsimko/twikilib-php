1fb945bfb7e38e4934db29d12af0dd459713210b
viliam.simko@gmail.com <viliam.simko@gmail.com@e8d5bb4a-7aaa-36a1-c079-f33c94fe2e21>
2011-11-25 00:42:23 +0000

    Added support for value slots inside wiki text which is useful for ajax-based applications.

--- src-api/twikilib/nodes/TopicTextNode.php
+++ src-api/twikilib/nodes/TopicTextNode.php
@@ -10,17 +10,17 @@ use twikilib\core\IRenderable;
  * @author Viliam Simko
  */
 class TopicTextNode implements IRenderable {
-	
+
 	/**
 	 * Cloning not allowed for this class.
 	 */
 	final private function __clone() {}
-	
+
 	/**
 	 * @var string
 	 */
 	private $text = '';
-	
+
 	/**
 	 * Replaces the content of the text node.
 	 * @param string $newText
@@ -29,7 +29,7 @@ class TopicTextNode implements IRenderable {
 		assert( is_string($newText) );
 		$this->text = $newText;
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
 	 * @see twikilib\core.IRenderable::toWikiString()
@@ -37,7 +37,7 @@ class TopicTextNode implements IRenderable {
 	final public function toWikiString() {
 		return rtrim($this->text)."\n";
 	}
-	
+
 	/**
 	 * Extracts all variables set inside the topic text.
 	 * In TWiki, local variables can using: '   * Set VARNAME = value' notation.
@@ -46,7 +46,7 @@ class TopicTextNode implements IRenderable {
 	final public function getLocalVariablesFromText() {
 
 		$result = array();
-		
+
 		if( preg_match_all('/\n(   )+\* Set ([a-zA-Z0-9_]+)\s*=\s*(.*)\n/', $this->text, $match) ) {
 			foreach( array_keys($match[2]) as $idx) {
 				$result[] = (object) array(
@@ -54,24 +54,24 @@ class TopicTextNode implements IRenderable {
 					'value'	=> $match[3][$idx] );
 			}
 		}
-		
+
 		return $result;
 	}
-	
+
 	/**
 	 * Extract all tables from the topic text.
 	 * Only the simple table format is supported:
-	 * 
+	 *
 	 * | *header* | *header* | *...* |
 	 * |   cell   |   cell   |  ...  |
 	 * |   cell   |   cell   |  ...  |
 	 * ...
-	 * 
+	 *
 	 * @return array of Table
 	 */
 	final public function getTablesFromText() {
 		$tables = array();
-		
+
 		// splitting the text by continuous table definitions
 		// the "\n" added at the beginning of the text is needed when the first table starts
 		// from the very beginning of the text
@@ -88,55 +88,75 @@ class TopicTextNode implements IRenderable {
 					$tableData = array();
 				}
 			}
-			
+
 			if( $tableData ) {
 				$tables[] = new Table($tableData);
 			}
 		}
-		
+
 		return $tables;
 	}
 
 	/**
 	 * TODO: tree structure of sections instead of a list
+	 * TODO: after updating a section, the changes are not propagated to the topic text
 	 * @return array of TextSection
 	 */
 	final public function getTextSections() {
 		$possibleSections = preg_split(
 			'/\n---([\+]{1,6})(.*)\n/', "\n".$this->text, -1, PREG_SPLIT_DELIM_CAPTURE);
-		
+
 		$sections = array();
 		for($i=1; $i<count($possibleSections); $i+=3) {
-			$sectionLevel = strlen( $possibleSections[$i] );
+			//$sectionLevel = strlen( $possibleSections[$i] );
 			$sectionName = trim($possibleSections[$i+1]);
 			$sectionText = $possibleSections[$i+2];
 			$sections[] = new TextSection($sectionName, $sectionText);
 		}
 		return $sections;
 	}
-	
+
 	/**
 	 * Indicates whether the last replaceText operation changed the topic text.
 	 * You need to manually reset this value to false.
 	 * @var boolean
 	 */
 	public $isTextChanged = false;
-	
+
 	/**
-	 * Appends some text at the end of the existing topic text.
+	 * Replaces some text within the topic topic text based on a regular expression.
+	 * This can be used for appending, updating or deleting content of a topic.
+	 * When the applied replacement has changed the text, a flag $isTextChanged is
+	 * set to true.
+	 *
+	 * Note: Newlines are treated in a special way. Make sure you use the right
+	 * pattern modifier.
+	 *
+	 * Examples:
+	 * - append text at the end of the whole text: /$/D
+	 * - append text at the end of each line: /$/m
+	 * - prepend text before the whole text: /^/
+	 * - replace the whole text: /^.*$/D
+	 *
+	 * @see http://www.php.net/manual/en/reference.pcre.pattern.modifiers.php
 	 * @param string $regexPattern
-	 * @param string $replacementText
+	 * @param string $replacementTextOrCallable
 	 * @return void
 	 */
-	final public function replaceText($regexPattern, $replacementText) {
-		$newText = preg_replace($regexPattern, $replacementText, $this->text);
-		
+	final public function replaceText($regexPattern, $replacementTextOrCallable) {
+		assert(is_string($regexPattern));
+		assert(is_string($replacementTextOrCallable) || is_callable($replacementTextOrCallable));
+
+		$newText = is_callable($replacementTextOrCallable)
+			? preg_replace_callback($regexPattern, $replacementTextOrCallable, $this->text)
+			: preg_replace($regexPattern, $replacementTextOrCallable, $this->text);
+
 		if($newText != $this->text) {
 			$this->text = $newText;
 			$this->isTextChanged = true;
 		}
 	}
-	
+
 	/**
 	 * @param string $sectionName
 	 * @return twikilib\fields\TextSection
@@ -148,6 +168,64 @@ class TopicTextNode implements IRenderable {
 				return $section;
 		}
 		return null;
+	}
+
+	/**
+	 * @param string $slotId
+	 * @return string
+	 */
+	private function hashSlotId($slotId) {
+		return md5($slotId);
+	}
+
+	/**
+	 * Creates a value surrounded by a token which can be easily
+	 * identified within the topic text and replaced.
+	 * The surrounding tokens will not be visible to the user.
+	 *
+	 * @param string $slotId
+	 * @param string $initialValue
+	 * @return string
+	 */
+	final public function createSlot($slotId, $initialValue='') {
+		assert(is_string($slotId));
+		assert(!empty($slotId));
+		assert(is_string($initialValue));
+
+		$hash = $this->hashSlotId($slotId);
+		return "<!--$hash-->$initialValue<!--$hash-->";
+	}
+
+	/**
+	 * Changes the value of a given slot which was previously
+	 * created by the createSlot() method.
+	 * The value can be replaced by a new value or it can be
+	 * processed by a callback/lambda function:
+	 *
+	 * <code>
+	 * function($matches) {
+	 *   return dosomething( $matches['value'] );
+	 * }
+	 * </code>
+	 *
+	 * @param string $slotId
+	 * @param mixed $valueOrCallable
+	 */
+	final public function updateSlot($slotId, $valueOrCallable) {
+		assert(is_string($slotId));
+		assert(!empty($slotId));
+		assert(is_string($valueOrCallable) || is_callable($valueOrCallable));
+
+		$hash = $this->hashSlotId($slotId);
+
+		$this->replaceText(
+			"/(?P<begin><!--$hash-->)(?P<value>.*?)(?P<end><!--$hash-->)/Ds", // (.*?) = non-greedy
+			is_callable($valueOrCallable)
+			? function($matches) use ($valueOrCallable) {
+				return $matches['begin'].$valueOrCallable($matches).$matches['end'];
+			}
+			: '$1'.$valueOrCallable.'$3'
+		);
 	}
 }
 ?>

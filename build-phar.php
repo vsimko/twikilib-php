cff1409a4533d1fc326336f70345b40569577bd1
Viliam Simko <viliam.simko@gmail.com>
2019-02-06 11:52:54 +0100

    reformatted in intellij idea

--- build-phar.php
+++ build-phar.php
@@ -14,55 +14,55 @@
 // (The config file is located in /etc/php5/conf.d/suhosin.ini on Ubuntu)
 
 // accepting a single optional parameter
-if( ! empty($argv[1])) {
-	chdir( $argv[1] );
+if (!empty($argv[1])) {
+  chdir($argv[1]);
 }
 
-$PKGBASE = basename( getcwd() );
+$PKGBASE = basename(getcwd());
 $SRCPREFIX = 'src'; // Directories with this prefix are regarded as containing source code of an application
 $DISTDIR = 'dist';  // The generated PHAR files will be saved into this directory (one PHAR per source folder)
 
-foreach(glob($SRCPREFIX.'*') as $SRCNAME) {
+foreach (glob($SRCPREFIX . '*') as $SRCNAME) {
 
-	$parts = preg_split('/[^a-zA-Z]+/', $SRCNAME );
-	$parts = array_filter($parts); // remove empty parts
-	array_shift($parts);
-	array_unshift($parts, $PKGBASE);
+  $parts = preg_split('/[^a-zA-Z]+/', $SRCNAME);
+  $parts = array_filter($parts); // remove empty parts
+  array_shift($parts);
+  array_unshift($parts, $PKGBASE);
 
-	$PKGNAME = implode('-', $parts);
-	$DISTNAME = $DISTDIR.DIRECTORY_SEPARATOR.$PKGNAME.'.phar';
+  $PKGNAME = implode('-', $parts);
+  $DISTNAME = $DISTDIR . DIRECTORY_SEPARATOR . $PKGNAME . '.phar';
 
-	@unlink($DISTNAME);
-	$phar = new Phar( $DISTNAME );
-	$phar->setAlias($PKGNAME);
-	$phar->buildFromDirectory( $SRCNAME, '/^(.(?!\.svn))*$/' ); // exclude .svn subdirectories
+  @unlink($DISTNAME);
+  $phar = new Phar($DISTNAME);
+  $phar->setAlias($PKGNAME);
+  $phar->buildFromDirectory($SRCNAME, '/^(.(?!\.svn))*$/'); // exclude .svn subdirectories
 
-	// find stubs
-	foreach(glob("$SRCNAME/*.php") as $entry) {
-		$filecontent = file_get_contents($entry);
-		if( preg_match('/^[^\n]*@pharstub[^\n]*\n/', $filecontent) ) {
-			$pharstub = basename($entry);
-		}
+  // find stubs
+  foreach (glob("$SRCNAME/*.php") as $entry) {
+    $filecontent = file_get_contents($entry);
+    if (preg_match('/^[^\n]*@pharstub[^\n]*\n/', $filecontent)) {
+      $pharstub = basename($entry);
+    }
 
-		if( preg_match('/^[^\n]*@pharwebstub[^\n]*\n/', $filecontent) ) {
-			$pharwebstub = basename($entry);
-		}
-	}
+    if (preg_match('/^[^\n]*@pharwebstub[^\n]*\n/', $filecontent)) {
+      $pharwebstub = basename($entry);
+    }
+  }
 
-	if( empty($pharstub) ) {
-		$pharstub = 'index.php';
-	}
+  if (empty($pharstub)) {
+    $pharstub = 'index.php';
+  }
 
-	if( empty($pharwebstub) ) {
-		$pharwebstub = 'index.php';
-	}
+  if (empty($pharwebstub)) {
+    $pharwebstub = 'index.php';
+  }
 
-	$phar->setStub( $phar->createDefaultStub($pharstub, $pharwebstub) );
+  $phar->setStub($phar->createDefaultStub($pharstub, $pharwebstub));
 
-	// Uncomment this if you want to explore the content of a PHAR using ZIP tools
-	//$phar->convertToExecutable(Phar::ZIP);
+  // Uncomment this if you want to explore the content of a PHAR using ZIP tools
+  //$phar->convertToExecutable(Phar::ZIP);
 
-	echo "PHAR written to '".$phar->getPath()."' using alias '".$phar->getAlias()."'\n";
-	echo " - stub file is : $pharstub\n";
-	echo " - web stub file is : $pharwebstub\n";
+  echo "PHAR written to '" . $phar->getPath() . "' using alias '" . $phar->getAlias() . "'\n";
+  echo " - stub file is : $pharstub\n";
+  echo " - web stub file is : $pharwebstub\n";
 }
